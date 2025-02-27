cc21b4a38dc0bcccb41cac128814aa6df7b070b7
alves <{>
2025-02-27 21:53:52 +0900

    ssstest

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-          sssss
+          ssssssssss
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
